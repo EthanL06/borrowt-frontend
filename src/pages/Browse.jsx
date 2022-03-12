e6098822b1371c61850649adc6d5581c852ccb20
EthanL06 <ethanlanting@gmail.com>
2022-03-12 14:23:56 -0600

    current

---
+++ src/pages/Browse.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Browse = () => {
+  return (
+    <div>
+      <div className="text-5xl font-bold">
+        Browse <span className="text-purple-600">NFTs</span>
+        <div className="w-5 bg-purple-600">test</div>
+      </div>
+    </div>
+  );
+};
